ba7a2253956c4df0b3c73275dc27df4547e0eb45
Maxime Fabas <m.fabas@liberation.fr>
2019-01-24 12:31:16 +0100

    Add .lblb-paragraph and .lblb-annotation inner links styles

--- src/index.js
+++ src/index.js
@@ -161,7 +161,7 @@ class App extends React.Component {
           <div className='lblb-components__comp-slot'>
             <div className='lblb-components__component-variant'>!literary</div>
             <div className='lblb-components__component'>
-              <Paragraph {...txtLvl}>Praesent ultrices et elit eu pharetra. Aliquam vehicula maximus mauris vel accumsan. Cras accumsan quam rutrum, commodo nunc quis, laoreet mauris. Nam ipsum ipsum, lobortis et fermentum in, placerat ac ligula. Duis at luctus lorem. Suspendisse a eros neque. Sed maximus leo nec eros finibus faucibus. Vivamus hendrerit aliquet ex eget varius. Donec posuere massa magna, ut lobortis elit viverra quis. Maecenas gravida magna at vestibulum iaculis.</Paragraph>
+              <Paragraph {...txtLvl}>Praesent ultrices et elit eu pharetra. Aliquam vehicula <a href="#">maximus mauris vel accumsan</a>. Cras accumsan quam rutrum, commodo nunc quis, laoreet mauris. Nam ipsum ipsum, lobortis et fermentum in, placerat ac ligula. Duis at luctus lorem. Suspendisse a eros neque. Sed maximus leo nec eros finibus faucibus. Vivamus hendrerit aliquet ex eget varius. Donec posuere massa magna, ut lobortis elit viverra quis. Maecenas gravida magna at vestibulum iaculis.</Paragraph>
             </div>
             <div className='lblb-components__component-props'>
               children: inline tags and text<br />
@@ -171,7 +171,7 @@ class App extends React.Component {
           <div className='lblb-components__comp-slot'>
             <div className='lblb-components__component-variant'>literary</div>
             <div className='lblb-components__component'>
-              <Paragraph {...txtLvl} literary>Praesent ultrices et elit eu pharetra. Aliquam vehicula maximus mauris vel accumsan. Cras accumsan quam rutrum, commodo nunc quis, laoreet mauris. Nam ipsum ipsum, lobortis et fermentum in, placerat ac ligula. Duis at luctus lorem. Suspendisse a eros neque. Sed maximus leo nec eros finibus faucibus. Vivamus hendrerit aliquet ex eget varius. Donec posuere massa magna, ut lobortis elit viverra quis. Maecenas gravida magna at vestibulum iaculis.</Paragraph>
+              <Paragraph {...txtLvl} literary>Praesent ultrices et elit eu pharetra. Aliquam vehicula <a href="#">maximus mauris vel accumsan</a>. Cras accumsan quam rutrum, commodo nunc quis, laoreet mauris. Nam ipsum ipsum, lobortis et fermentum in, placerat ac ligula. Duis at luctus lorem. Suspendisse a eros neque. Sed maximus leo nec eros finibus faucibus. Vivamus hendrerit aliquet ex eget varius. Donec posuere massa magna, ut lobortis elit viverra quis. Maecenas gravida magna at vestibulum iaculis.</Paragraph>
             </div>
             <div className='lblb-components__component-props'>
               children: inline tags and text<br />
@@ -213,7 +213,7 @@ class App extends React.Component {
           <div className='lblb-components__comp-slot'>
             <div className='lblb-components__component-variant'>!literary</div>
             <div className='lblb-components__component'>
-              <Annotation {...txtLvl}>The quick brown fox jumps over the lazy dog.</Annotation>
+              <Annotation {...txtLvl}>The quick brown fox jumps over <a href="#">the lazy dog.</a></Annotation>
             </div>
             <div className='lblb-components__component-props'>
               children: inline tags and text<br />
@@ -223,7 +223,7 @@ class App extends React.Component {
           <div className='lblb-components__comp-slot'>
             <div className='lblb-components__component-variant'>literary</div>
             <div className='lblb-components__component'>
-              <Annotation {...txtLvl} literary>The quick brown fox jumps over the lazy dog.</Annotation>
+              <Annotation {...txtLvl} literary>The quick brown fox jumps over <a href="#">the lazy dog.</a></Annotation>
             </div>
             <div className='lblb-components__component-props'>
               children: inline tags and text<br />
