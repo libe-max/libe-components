b8fdf9559daf2315e3e986c594a4e75c9bbc46ec
Maxime Fabas <m.fabas@liberation.fr>
2019-04-09 14:26:15 +0200

    Update build.sh in order to compile .globals.js

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash/ -e
 
 rm -rf ./lib &&
-NODE_ENV=production babel ./src/components -d ./lib -D 
+NODE_ENV=production babel ./src/components -d ./lib -D --include-dot-files --no-comments --compact --minified
+NODE_ENV=production babel ./src/components/.globals.js -o ./lib/.globals.js --include-dot-files --no-comments --compact --minified
