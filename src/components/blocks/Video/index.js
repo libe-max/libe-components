80ebb19aea51b2d9e7f7d285f93813d3c73d66d0
libe <libe@ger12075004.local>
2019-11-27 14:21:25 +0100

    update video component

--- src/components/blocks/Video/index.js
+++ src/components/blocks/Video/index.js
@@ -29,29 +29,34 @@ export default class Video extends Component {
   }
 
   guessOriginAndId (src) {
-    let origin
-    let id
     if (src.includes("youtube")) {
-      origin = 'youtube'
-      id = src.split("&")[0].split("?v=")[1]
+      return {
+        origin: 'youtube',
+        id: src.split("&")[0].split("?v=")[1]
+      }
     }
-    else if (src.includes("dailymotion")) {
-      origin = 'dailymotion'
-      id = src.split("&")[0].split("/").pop()
+    if (src.includes("dailymotion")) {
+      return {
+        origin: 'dailymotion',
+        id: src.split("&")[0].split("/").pop()
+      }
     }
-    else if (src.includes("ina.fr/video")) {
-      origin = 'ina'
-      id = this.props.src.split("&")[0].split("/").pop()
+    if (src.includes("ina.fr/video")) {
+      return {
+        origin:'ina',
+        id: this.props.src.split("&")[0].split("/").pop()
+      }
     }
-    else if (src.includes("ina")) {
-      origin = 'ina'
-      id = null
+    if (src.includes("ina")) {
+      return {
+        origin: 'ina',
+        id: null
+      }
     }
-    else {
-      origin = 'file'
-      id = null
+    return {
+      origin: 'file',
+      id: null
     }
-    return { origin, id }
   }
 
   buildVideoEmbedUrl (origin, id) {
@@ -62,6 +67,8 @@ export default class Video extends Component {
         return `https://www.dailymotion.com/embed/video/${id}`
       case 'ina':
         return `https://player.ina.fr/player/embed/${id}/1/1b0bd203fbcd702f9bc9b10ac3d0fc21/wide/1`
+      default:
+        return ''
     }
   }
 
@@ -73,35 +80,29 @@ export default class Video extends Component {
   render () {
     const { c, props } = this
 
-    /* Assign classes */
-    const classes = [c]
-
     const { origin, id } = this.guessOriginAndId(props.src)
 
-    if (!origin || (origin != 'file' && !id)) {
-      classes.push(`${c}_unknown-source`)
-      return <div className={classes.join(' ')}>
-        <Paragraph>Source inconnue</Paragraph>
-      </div>
-    }
-    else if (origin == 'file') {
-      classes.push(`${c}_file`)
-      return <div className={classes.join(' ')}>
-        <video controls {...props} />
-      </div>
-    }
-    else {
-      classes.push(`${c}_${origin}`)
-      /* Display component */
-      return <div className={classes.join(' ')} style={{ paddingTop: `${100 /props.ratio}%` }}>
-        <iframe
-          title={`${origin}-${id}`}
-          src={this.buildVideoEmbedUrl(origin, id)}
-          scrolling='no'
-          allowFullScreen>
-        </iframe>
-      </div>
-    }
+    const unknown = !origin || (origin !== 'file' && !id)
+
+    /* Assign classes */
+    const classes = [c]
+    if (unknown) classes.push(`${c}_unknown-origin`)
+    else classes.push(`${c}_${origin}`)
+
+  return <div
+    className={classes.join(' ')}
+    style={!unknown && origin !== 'file' ? { paddingTop: `${100 /props.ratio}%` } : {}}>
+        {unknown && <Paragraph>Source inconnue</Paragraph>}
+        {origin === 'file' && <video controls {...props} />}
+        {!unknown && origin !== 'file' && (
+          <iframe
+            title={`${origin}-${id}`}
+            src={this.buildVideoEmbedUrl(origin, id)}
+            scrolling='no'
+            allowFullScreen>
+          </iframe>
+        )}
+        </div>
   }
 }
 
