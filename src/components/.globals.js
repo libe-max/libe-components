b22c6f2b4bbfe6f66349ad05347d4e5ccb9fcc30
Maxime Fabas <maxime.fabas@pm.me>
2019-12-11 18:07:59 +0100

    fix wrong statics url when away from localhost

--- src/components/.globals.js
+++ src/components/.globals.js
@@ -1,18 +1,18 @@
 const env = process.env.NODE_ENV
 
-const devStaticsRootUrl = 'http://localhost:3003'
+const devStaticsRootUrl = `${window.location.protocol}//${window.location.hostname}:3003`
 const prodStaticsRootUrl = 'https://www.liberation.fr/apps/static'
 const staticsRootUrl = env === 'production'
   ? prodStaticsRootUrl
   : devStaticsRootUrl
 
-const devApiRootUrl = 'http://localhost:3004/api'
+const devApiRootUrl = `${window.location.protocol}//${window.location.hostname}:3004/api`
 const prodApiRootUrl = 'https://libe-labo-2.site/api'
 const apiRootUrl = env === 'production'
   ? prodApiRootUrl
   : devApiRootUrl
 
-const devProxyRootUrl = 'http://localhost:3004/proxy'
+const devProxyRootUrl = `${window.location.protocol}//${window.location.hostname}:3004/proxy`
 const prodProxyRootUrl = 'https://libe-labo-2.site/proxy'
 const proxyRootUrl = env === 'production'
   ? prodProxyRootUrl
