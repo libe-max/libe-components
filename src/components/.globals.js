edaec1ff2c8efb9f0ac97d356a3d3f3853b88cdf
Maxime Fabas <maxime.fabas@pm.me>
2020-04-08 15:04:56 +0200

    fix undefined window when running build

--- src/components/.globals.js
+++ src/components/.globals.js
@@ -1,18 +1,21 @@
 const env = process.env.NODE_ENV
 
-const devStaticsRootUrl = `${window.location.protocol}//${window.location.hostname}:3003`
+const currentProtocol = typeof window !== 'undefined' ? window.location.protocol : 'http:'
+const currentHostname = typeof window !== 'undefined' ? window.location.hostname : 'localhost'
+
+const devStaticsRootUrl = `${currentProtocol}//${currentHostname}:3003`
 const prodStaticsRootUrl = 'https://www.liberation.fr/apps/static'
 const staticsRootUrl = env === 'production'
   ? prodStaticsRootUrl
   : devStaticsRootUrl
 
-const devApiRootUrl = `${window.location.protocol}//${window.location.hostname}:3004/api`
+const devApiRootUrl = `${currentProtocol}//${currentHostname}:3004/api`
 const prodApiRootUrl = 'https://libe-labo-2.site/api'
 const apiRootUrl = env === 'production'
   ? prodApiRootUrl
   : devApiRootUrl
 
-const devProxyRootUrl = `${window.location.protocol}//${window.location.hostname}:3004/proxy`
+const devProxyRootUrl = `${currentProtocol}//${currentHostname}:3004/proxy`
 const prodProxyRootUrl = 'https://libe-labo-2.site/proxy'
 const proxyRootUrl = env === 'production'
   ? prodProxyRootUrl
